76a78b57865c47a4d9abd57b3b8b1a640c814445
Vasily Nikulichev <v.nikulichev@404-group.com>
2019-05-29 13:07:34 +0300

    test

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -8,8 +8,8 @@ function pow(m, n) {
     let number = m;
 
     for(let i = 0; i < n; i++) {
-        if (i>0) {
-            number = number*m;
+        if (i > 0) {
+            number = number * m;
         }
     }
 
