611fe58863376f7830f03688d3cf729f34bb3ba7
javascript-ru <47786167+jsru@users.noreply.github.com>
2019-05-29 12:03:12 +0200

    Added task Найти min/max в строке

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,16 +6,6 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-    let myArray = str.split(" ").join().split(',');
-    let min = parseFloat(myArray[0]);
-    let max = min;
 
-    for (let i in myArray) {
-        myArray[i] = parseFloat(myArray[i]);
-        if (myArray[i] > max) max = myArray[i];
-        if (myArray[i] < min) min = myArray[i];
-    }
-
-    return {min, max};
 }
 
